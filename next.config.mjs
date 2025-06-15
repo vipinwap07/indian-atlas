d9ecb6e6d98f3f2e7b089ff6289523afbc716af6
Vipin Kumar <vipinwap07@gmail.com>
2025-06-15 18:26:57 +0530

    Updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/indian-atlas",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
